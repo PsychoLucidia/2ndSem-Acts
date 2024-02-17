48de26000882339738747d724adce2b134dd92a0
PsychoLucidia <89222930+PsychoLucidia@users.noreply.github.com>
2024-02-17 10:33:43 +0800

    New Scripts

---
+++ Assets/Scripts/TestScripts/Character.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character
+{
+    public Character(string name, int HP, int MP)
+    {
+        this.name = name;
+        this.HP = HP;
+
+    }
+}
+
